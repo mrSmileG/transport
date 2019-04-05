16a3f6ab68f8bd7b7b0229f0de9cc300468523d8
Gregory Golovko <golovko.greg@gmail.com>
2019-04-05 11:38:24 +0300

    Added started message to the main page get request

--- main.go
+++ main.go
@@ -5,20 +5,26 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/mrsmileg/transp/problem"
 )
 
-func getSolve(w http.ResponseWriter, r *http.Request) {
+func postSolve(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var problem problem.Problem
 	err := json.NewDecoder(r.Body).Decode(&problem)
 
 	if err != nil {
-
+		json.NewEncoder(w).Encode(err)
 	}
 
 	json.NewEncoder(w).Encode(problem.GetSolution())
+}
+
+func getSolve(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode("Welcome to the Transportation Problem Solver. Please give me JSON task to my body with POST method")
 }
 
 func main() {
@@ -26,7 +32,10 @@ func main() {
 	if port == "" {
 		port = "8008"
 	}
+	allowedOrigins := handlers.AllowedOrigins([]string{"*"})
+	allowedMethods := handlers.AllowedMethods([]string{"POST", "GET"})
 	router := mux.NewRouter()
-	router.HandleFunc("/transport/", getSolve).Methods("POST")
-	problem.Logger.Fatal(http.ListenAndServe(":"+port, router))
+	router.HandleFunc("/", getSolve).Methods("GET")
+	router.HandleFunc("/transport/", postSolve).Methods("POST")
+	problem.Logger.Fatal(http.ListenAndServe(":"+port, handlers.CORS(allowedOrigins, allowedMethods)(router)))
 }
